a5300c15020cfb47949ccca3616d384b17878461
Garrett Lietzen <glietzen@gmail.com>
2018-04-11 20:17:38 -0500

    now really finished.

--- bamazonManager.js
+++ bamazonManager.js
@@ -15,7 +15,7 @@ let getCommand = () => {
     console.log('Enter a command number:\n1: Products For Sale\n2: View Low Inventory\n3: Add to Inventory\n4: Add New Product');
     prompt.get(['command'], (err,result) => {
         if (result.command == 1) {
-            listProducts();
+            listProducts(1);
         } else if (result.command == 2) {
             viewLowInventory();
         } else if (result.command == 3) {
@@ -29,7 +29,8 @@ let getCommand = () => {
     });
 }
 
-let listProducts = () => {
+let listProducts = (a) => {
+    if (a === 1) {
     console.log('Listing All Products');
     console.log('--------------------------')
     console.log("Here's our product selection:\n");
@@ -38,8 +39,22 @@ let listProducts = () => {
         for (var i = 0; i < res.length; i++) {
             console.log(`Product ID: ${res[i].id} | Product Name: ${res[i].product_name} | Price: $${res[i].price} | Quantity: ${res[i].quantity}`);
         }
-    })
-    setTimeout(getCommand,1500);
+        setTimeout(getCommand,1500);
+    })} else if (a === 3) {
+        console.log('Listing All Products');
+        console.log('--------------------------')
+        console.log("Here's our product selection:\n");
+        connection.query("SELECT * FROM products", function (err, res) {
+            if (err) throw err;
+            for (var i = 0; i < res.length; i++) {
+                console.log(`Product ID: ${res[i].id} | Product Name: ${res[i].product_name} | Price: $${res[i].price} | Quantity: ${res[i].quantity}`);
+            }
+        });
+        prompt.get(['itemNumber', 'newQuantity'], (err, b) => {
+            updateInventory(b.itemNumber, b.newQuantity);
+        })
+    }
+    
 }
 
 let viewLowInventory = () => {
@@ -56,6 +71,27 @@ let viewLowInventory = () => {
 }
 
 let addToInventory = () => {
+    console.log('What item would you like to edit?');
+    listProducts(3);
+}
+
+let updateInventory = (a, b) => {
+    var query = connection.query(
+        "UPDATE products SET ? WHERE ?",
+        [
+            {
+                quantity: b
+            },
+            {
+                id: a
+            }
+        ],
+        console.log(`Quatity for ${a} is now updated to ${b}\n`)
+    );
+    setTimeout(getCommand, 1500);
+}
+
+let addNewProduct = () => {
     console.log('What would you like to add?');
     prompt.get(['pName', 'pDept', 'pPrice', 'pQuantity'], (err, res) => {
         if (err) throw err;
@@ -74,12 +110,6 @@ let addToInventory = () => {
                 })
         }
     })
-    
-}
-
-let addNewProduct = () => {
-    console.log('add product');
-    setTimeout(getCommand,1500);
 }
 
 
